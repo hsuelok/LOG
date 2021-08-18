597732dada05471187cac59baeaea553d5f95179
HsuelokMBP <hsuelok@icloud.com>
2021-08-18 21:55:20 +0800

    macro pos changed

--- LOG.cpp
+++ LOG.cpp
@@ -2,16 +2,17 @@
 // Created by Kepler on 2021/8/18.
 //
 
-#include "LOG.h"
-
-// LOG Level: FATAL, ERROR, WARN, INFO, DEBUG, TRACE.
-
 #define DEBUG(debug) LOG4CPLUS_DEBUG(logger_, LOG4CPLUS_TEXT(debug))
 #define INFO(info) LOG4CPLUS_DEBUG(logger_, LOG4CPLUS_TEXT(info))
 #define ERROR(error) LOG4CPLUS_ERROR(logger_, LOG4CPLUS_TEXT(error))
 #define FATAL(fatal) LOG4CPLUS_FATAL(logger_, LOG4CPLUS_TEXT(fatal))
 #define TRACE(trace) LOG4CPLUS_TRACE(logger_, LOG4CPLUS_TEXT(trace))
 #define WARN(warn) LOG4CPLUS_WARN(logger_, LOG4CPLUS_TEXT(warn))
+
+#include "LOG.h"
+
+// LOG Level: FATAL, ERROR, WARN, INFO, DEBUG, TRACE.
+
 
 using namespace log4cplus;
 
